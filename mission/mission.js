e88cdda1ee94141c7653b3e9e4b2237d99d8005e
GageDorl <gagedorlando@gmail.com>
2025-01-13 20:16:43 -0700

    finished mission statement

--- mission/mission.js
+++ mission/mission.js
@@ -2,9 +2,11 @@ const themeSelector = document.querySelector('select');
 function changeTheme(){
     if(themeSelector.value=='dark'){
         document.querySelector('body').setAttribute('class','dark');
+        document.querySelector('img').setAttribute('src','byui-logo_white.webp');
     }
     else{
-        document.querySelector('body').setAttribute('class', '')
+        document.querySelector('body').setAttribute('class', '');
+        document.querySelector('img').setAttribute('src','byui-logo_blue.webp');
     }
 }
 themeSelector.addEventListener('change', changeTheme);
